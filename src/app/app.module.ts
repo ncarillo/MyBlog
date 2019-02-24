a7e80c0c812854187b85aa7ffd85554b2b1aa256
Nico <nicolas.carillo@gmail.com>
2019-02-24 20:02:09 +1100

    Structure du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { SinglePostComponent } from './post-list/single-post/single-post.component';
+import { PostFormComponent } from './post-list/post-form/post-form.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    PostListComponent,
+    SinglePostComponent,
+    PostFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
